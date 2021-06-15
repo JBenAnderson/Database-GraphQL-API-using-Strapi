f811529c6ab827a21ed943a8d7247bdd3eaff02c
J Ben Anderson <revovisions@gmail.com>
2021-06-15 14:28:38 -0400

    added restaurants folder

---
+++ restaurants/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1f804a14cf3a7dcae58ecae7f61eb9ac'),
+    },
+  },
+});
---
+++ restaurants/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2e689b05-cb2c-42b6-8d44-076f8e5b89e3'
+};
